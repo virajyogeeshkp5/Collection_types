c6e05b2e1c878a9d9a243b551939ac2d7cb07047
YOGEESH K P <yogeesh793@gmail.com>
2024-07-06 12:34:22 +0530

    Add files via upload

---
+++ Collection_types.py
@@ -0,0 +1,34 @@
+# Collection Types
+# 1
+names = ['Alice','Bob']
+names.append('sa')
+print(names)
+
+# 2
+names.insert(1,'ok')
+print(names)
+
+#3
+names.remove('Bob')
+print(names)
+
+#4
+names.index('Alice')
+print(names)
+
+#5
+print(len(names))
+
+#6
+a = [1,1,1,2,3]
+print(a.count(1))
+
+#7
+print(a[ : :-1])
+
+#8
+a.pop()
+print(a)
+
+
+
